935b0af4aebfa005ba68df3f10c58a5e29b3c8de
Kesava Karri <kkeshav1998@gmail.com>
2020-09-04 17:47:27 +0530

    addedTest

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) { //t is the test handler
 	d := newDeck()
@@ -8,4 +11,27 @@ func TestNewDeck(t *testing.T) { //t is the test handler
 	if len(d) != 16 {
 		t.Errorf("Expected length of 16 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades, got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Hearts" {
+		t.Errorf("Expected Four of Hearts, got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected length of 16, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
